77cffe964083a127b2b8374da12f59075f2a586f
Kevin Vollmer <works.kvollmer@gmail.com>
2014-08-16 16:42:26 -0400

    all data from page scrape (uses temps)

--- einibl.sh
+++ einibl.sh
@@ -1,12 +1,15 @@
+if [[ -z $@ ]]; then
+  exit
+fi
 str=$@
 m=${str// /+}
-urll=$(curl -sL http://nibl.co.uk/bots.php?search=$m)
+url=$(wget -q -O- http://nibl.co.uk/bots.php?search=$m | sed 's/<td class="\(botname\|packnumber\|filesize\|filename\)">/<td class="\1">\n/g')
 bot="/html/body/div/div/div/table/tr/td[@class = 'botname']/text()"
 pack="/html/body/div/div/div/table/tr/td[@class = 'packnumber']/text()"
 size="/html/body/div/div/div/table/tr/td[@class = 'filesize']/text()"
 name="/html/body/div/div/div/table/tr/td[@class = 'filename']/text()"
-a=$(echo $urll | xmllint --html --xpath '/html/body/div/div/div/table/tr/td[@class = 'botname']/text()' --format -)
-b=$(echo $urll | xmllint --html --xpath '/html/body/div/div/div/table/tr/td[@class = 'filename']/text()' --format -)
-#c=$(echo $urll | xmllint --html --xpath '$size' --format -)
-#d=$(echo $urll | xmllint --html --xpath '$name' --format -)
-paste <(echo "$a") <(echo "$b") -d '|'
+echo "$url" | xmllint --html --xpath "$bot" --format -  | sed '/^\s*$/d' > a
+echo "$url" | xmllint --html --xpath "$pack" --format - | sed '/^\s*$/d' > b
+echo "$url" | xmllint --html --xpath "$size" --format - | sed '/^\s*$/d' > c
+echo "$url" | xmllint --html --xpath "$name" --format - | sed '/^\s*$/d' > d
+paste -d "|" a b c d
