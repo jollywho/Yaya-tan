10b34871ccc71fa813cf36797e23b0dc65e3666e
Kevin Vollmer <works.kvollmer@gmail.com>
2014-08-16 19:44:44 -0400

    xclip msg

--- einibl.sh
+++ einibl.sh
@@ -1,6 +1,7 @@
 if [[ -z $@ ]]; then
   exit
 fi
+
 red='\\e[0;31m'
 yellow='\\e[0;33m'
 cyan='\\e[0;36m'
@@ -8,15 +9,28 @@ green='\\e[1;32m'
 NC='\\e[0m'
 str=$@
 m=${str// /+}
+
 url=$(wget -q -O- http://nibl.co.uk/bots.php?search=$m | sed 's/<td class="\(botname\|packnumber\|filesize\|filename\)">/<td class="\1">\n/g')
 bot="/html/body/div/div/div/table/tr/td[@class = 'botname']/text()"
 pack="/html/body/div/div/div/table/tr/td[@class = 'packnumber']/text()"
 size="/html/body/div/div/div/table/tr/td[@class = 'filesize']/text()"
 name="/html/body/div/div/div/table/tr/td[@class = 'filename']/text()"
+
 echo -e "$url" | xmllint --html --xpath "$bot" --format - | sed '/^\s*$/d' | sed 's/^/'"${yellow}"'/' | sed 's/\s*$/'"${NC}"'/' > a
 echo -e "$url" | xmllint --html --xpath "$pack" --format - | sed '/^\s*$/d' | sed 's/^/'"${cyan}"'/' | sed 's/$/'"${NC}"'/' > b
 echo -e "$url" | xmllint --html --xpath "$size" --format - | sed '/^\s*$/d' | sed 's/^/'"${red}"'/' | sed 's/$/'"${NC}"'/' > c
 echo -e "$url" | xmllint --html --xpath "$name" --format - | sed '/^\s*$/d' | sed 's/^/'"${green}"'/' | sed 's/$/'"${NC}"'/' > d
-o=$(paste -d "|" a b c d)
+o=$(paste -d ";" a b c d)
 rm a b c d
-echo -e "$o"
+
+echo -e "$o" | less -N
+read item
+
+msg=$(echo -e "$o" | sed -n "${item}p")
+clip=$(echo "$msg" | perl -pe 's/\e\[?.*?[\@-~]//g')
+bot=$(echo -e "$clip" | cut -d ';' -f1)
+pack=$(echo -e "$clip" | cut -d ';' -f2)
+clip="/msg ${bot} xdcc send #${pack}"
+
+echo -e "$clip"
+echo $clip | xclip
