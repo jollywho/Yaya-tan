377f0bee28c73f5c4e34f2b2cb8330ae0517dde4
Kevin Vollmer <works.kvollmer@gmail.com>
2014-08-30 07:38:05 -0400

    readme n stuffs

--- lib/test.py
+++ lib/test.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 import socket
 
-server = "irc.freenode.net"
-channel = "#yayachan"
+server = "irc.rizon.net"
+channel = "#/g/sicp"
 botnick = "Yaya-tan"
 
 def ping():
@@ -13,12 +14,12 @@ def sendmsg(chan , msg):
 def joinchan(chan):
   ircsock.send("JOIN " + chan + "\n")
 
-def hello():
-  ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
+def hello(nick):
+  ircsock.send("PRIVMSG "+ channel +" :今晩は " + nick + "さま!\n")
 
 def commands(nick, channel, message):
     ircsock.send('PRIVMSG %s :%s お兄ちゃん\n' % (channel, message))
-    ircsock.send('PRIVMSG %s :%s お兄ちゃん\n' % (nick, message))
+    #ircsock.send('PRIVMSG %s :%s お兄ちゃん\n' % (nick, message))
 
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ircsock.connect((server, 6667))
@@ -32,14 +33,15 @@ while 1:
     ircmsg = ircmsg.strip('\n\r')
 
     print(ircmsg)
-    if ircmsg.find(' PRIVMSG ') != -1:
-        nick = ircmsg.split('!')[0][1:]
-        channel = ircmsg.split(' PRIVMSG ')[-1].split(' :')[0]
-        msg = ircmsg.split(' PRIVMSG ')[-1].split(' :')[1]
-        commands(nick, channel, msg)
+    #if ircmsg.find(' PRIVMSG ') != -1:
+    #    nick = ircmsg.split('!')[0][1:]
+    #    channel = ircmsg.split(' PRIVMSG ')[-1].split(' :')[0]
+    #    msg = ircmsg.split(' PRIVMSG ')[-1].split(' :')[1]
+    #    commands(nick, channel, msg)
 
     if ircmsg.find("Hello") != -1:
-        hello()
+        nick = ircmsg.split('!')[0][1:]
+        hello(nick)
 
     if ircmsg.find("PING :") != -1:
         ping()
