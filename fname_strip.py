1d383fb18546ae04f9efd91644ef51433a74f875
Kevin Vollmer <works.kvollmer@gmail.com>
2014-09-06 15:35:49 -0400

    regex update

--- fname_strip.py
+++ fname_strip.py
@@ -2,4 +2,4 @@ import sys
 import re
 name = sys.stdin.read()
 print(name)
-print(re.match("(?:\[.*?\])?_?(.+)_?.+(?:[_|-][0-9]{1,3}).*(?:\[.*?\])?", name).groups()[0])
+print(re.match("(?:\[.*?\])?[_ ]?(.+)(?:[_ -])+?.*?(?:[_ -][0-9]{1,3}).*(?:\[.*?\])?", name).groups()[0])
