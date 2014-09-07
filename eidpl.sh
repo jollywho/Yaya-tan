a31544be46ec53be176b4b7e225ba0bedd0f879f
Kevin Vollmer <works.kvollmer@gmail.com>
2014-09-07 06:58:06 -0400

    waiting msg

--- eidpl.sh
+++ eidpl.sh
@@ -1,9 +1,11 @@
 pend="/dev/sr0: writable, no read permission"
 res=$(sudo file -s /dev/sr0)
+printf "waiting..."
 while [[ $res == $pend ]]
 do
   res=$(sudo file -s /dev/sr0)
 done
+printf "done.\n"
 sudo mount /dev/sr0 /media/1
 a=$(find /media/1 -type f -printf x | wc -c)
 b=$(find /media/1 -type d -printf x | wc -c)
@@ -11,16 +13,8 @@ casper_path=/mnt/casper/chishiki/dvds
 if [[ $a -gt 0 || $b -gt 1 ]]; then
   printf "starting..."
   temp=$(mktemp -d $casper_path/XXXXX) && chmod 755 $temp
-  cp -rf /media/1/* $temp
-
-#find . -iname | while read -d $'\0' i
-#do
-#echo "${i}"
-#done
-
-  #if casper_path has subfolder
-    #mv subfolder to casper_path
-    #rm subfolder if empty
+  sudo cp -rf /media/1/* $temp
+  printf "done.\n"
   tput setaf 2
   banner -C --font=2 '##DONE##'
   tput setaf 5
