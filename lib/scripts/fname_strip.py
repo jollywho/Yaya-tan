d9b84f605f594e21274ea288936438208013cad5
Kevin Vollmer <works.kvollmer@gmail.com>
2014-09-10 18:20:02 -0400

    second iteration for regex

--- lib/scripts/fname_strip.py
+++ lib/scripts/fname_strip.py
@@ -2,5 +2,10 @@ import sys
 import re
 name = sys.stdin.read()
 print(name)
-ismatch = re.match("(?:\[.*?\])?[_ ]?(.+)(?:[_ -])+?.*?(?:[_ -][0-9]{1,3}).*(?:\[.*?\])?", name)
-print(ismatch.groups()[0]) if ismatch != None else None
+ismatch = re.match("(?:\[.*?\])?[_ ]?(.+).*?(?:[_ -][0-9]{1,3}).*(?:\[.*?\])?", name)
+if ismatch != None:
+    strip = re.match("(.+)[_ -].+", ismatch.groups()[0])
+    if strip != None:
+        print(strip.groups()[0])
+    else:
+        print(ismatch.groups()[0])
