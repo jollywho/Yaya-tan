b789bbaee981e1db9bf47e75ad64bee477ca72d6
Kevin Vollmer <works.kvollmer@gmail.com>
2014-09-15 20:49:39 -0400

    recursive 「再帰！」 matching

--- lib/scripts/eidirpop.sh
+++ lib/scripts/eidirpop.sh
@@ -1,4 +1,4 @@
-loc="/home/chishiki/xdcc"
+loc="/mnt/casper/chishiki/dvds"
 
 #loop on each directory in loc as top
 #loop on each subdirectory as second
@@ -6,6 +6,7 @@ loc="/home/chishiki/xdcc"
 #rename second to [](filename)[] of first item in its dir as ndir
 #if top contains ndir, move contents of ndir into top/ndir
 find $loc -iname '*.*' | while read file; do
+  echo "$file"
   basename "$file" | python fname_strip.py
 done
 
--- lib/scripts/fname_strip.py
+++ lib/scripts/fname_strip.py
@@ -5,18 +5,24 @@ name = sys.stdin.read()
 rep = re.sub("\[.*?\]", "", name)
 rep = re.sub("\(.*?\)", "", rep)
 
-def pull():
-    title = re.sub("[_ -]", "_", ismatch.groups()[0])
+match_type = \
+        ["(.+[^Ep]).*[._ -]([-0-9]{5})",
+        "(.+[^Ep]).*[._ -]([-0-9]{2,4})",
+        "(.+[^Ep])([-0-9]{2,4})"]
+
+def pull(m):
+    title = re.sub("[._ -]", "_", m.groups()[0])
     title = re.sub("^_", "", title)
     title = re.sub("__", "", title)
-    ep = re.sub("[_ ]", "", ismatch.groups()[1])
+    ep = re.sub("[_ ]", "", m.groups()[1])
     print(title)
     print(ep)
 
-ismatch = re.match("(.+[^Ep]).*[_ -]([-0-9]{5})", rep)
-if ismatch != None:
-    pull()
-else:
-    ismatch = re.match("(.+[^Ep]).*[_ -]([-0-9]{2,4})", rep)
-    if ismatch != None:
-        pull()
+def do_match(x):
+    m = re.match(match_type[x], rep)
+    if m != None:
+        pull(m)
+    else:
+        do_match(x+1) if x+1 < len(match_type) else None
+
+do_match(0)
