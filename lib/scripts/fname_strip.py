a73fe06a5ae773e8a3c6ba2bc4ad43e7a08162f6
Kevin Vollmer <works.kvollmer@gmail.com>
2014-09-19 19:29:40 -0400

    more filters and ep # limit reduced by 1

--- lib/scripts/fname_strip.py
+++ lib/scripts/fname_strip.py
@@ -7,20 +7,21 @@ rep = re.sub("\(.*?\)", "", rep)
 
 match_type = \
         [
-        "(.+[^Ep]).*[._ -]([0-9]{2,4}-[0-9]{2,4})",
-        "(.+[^Ep]).*[._ -]([0-9]{2,4})",
+        "(.+[^Ep]).*[._ -]([0-9]{2,3}-[0-9]{2,3})",
+        "(.+[^Ep]).*[._ -]([0-9]{2,3})",
         "(.+).*[._ -]([Ss][0-9]{2}[Ee][0-9]{2})",
         "(.+).*[._ -]([0-9]{1,2}[Xx][0-9]{1,2})",
-        "(.+[^Ep]).*([0-9]{2,4})",
+        "(.+[^Ep]).*([0-9]{2,3})",
+        "(.+[^Ep]).*(ED|OP[0-9]{1,2})",
         ]
 
 def pull(m):
     title = re.sub("[._ -]", "_", m.groups()[0])
     title = re.sub("^_", "", title)
     title = re.sub("__", "", title)
+    title = re.sub("_$", "", title)
     ep = re.sub("[_ ]", "", m.groups()[1])
-    print(title)
-    print(ep)
+    print(title.title(), " ", ep)
 
 def do_match(x):
     m = re.match(match_type[x], rep)
