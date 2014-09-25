ed7a5907a49f88b023b1dbd823ad5608eb6a81ea
Kevin Vollmer <works.kvollmer@gmail.com>
2014-09-25 17:44:09 -0400

    filename fix and install script

--- lib/scripts/eiyfs.sh
+++ lib/scripts/eiyfs.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/sh
 
-data=$(basename "${1}" | eidata)
+#todo: send "done {file}" msg to YQS
+
+#if file
+if [ -f "${1}" ]; then
+  data=$(basename "${1}" | eidata)
+else
+  data=${1}
+fi
+
 eiinsert ${data}
 eidirpop "${1}" ${data}
---
+++ lib/scripts/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+sudo cp eidata.py     /usr/bin/eidata
+sudo cp eidirpop.sh   /usr/bin/eidirpop
+sudo cp eiinsert.sh   /usr/bin/eiinsert
+sudo cp eiyfs.sh      /usr/bin/eiyfs
