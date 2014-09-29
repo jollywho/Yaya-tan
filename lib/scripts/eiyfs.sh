9f1743e06fc1c744a0dd531a9176acb776fee742
Kevin Vollmer <works.kvollmer@gmail.com>
2014-09-29 03:13:03 -0400

    Single-record goahead now works regardless of type

--- lib/scripts/eiyfs.sh
+++ lib/scripts/eiyfs.sh
@@ -9,22 +9,30 @@ else
   data=${1}
 fi
 
+#!!debug disabled
 #eiinsert ${data}
+
 if [ "$HOSTNAME" == casper ]; then
   EIIDIR='/usr/share/eii/lib'
 elif [ "$HOSTNAME" = melchior ]; then
   EIIDIR='/mnt/eii/lib'
 fi
+
 EIICMD="${EIIDIR}/eii.sh"
 dbdir=/home/chishiki/qp/einibl/anidb/bin/anidb.db
-sql="-t titles -c type,aid,title -f title -v '${name}'"
-res=$(. ${EIICMD} -s -db $dbdir $sql | grep "^1")
-if [ $(echo "${res}" | wc -l) -eq 1 ]; then
-  aid=$(echo $res | cut -d '|' -f2)
-  ruby anidb.rb $aid
-  #eii -u -x -t master -c episodecount -f name -v '${name}' -n adata0
-  #eii -u -x -t master -c date -f name -v '${name}' -n adata1
+sql="-t titles -c type,aid,title -f title -v ${name}"
+res=$(. ${EIICMD} -s -db $dbdir $sql)
+echo $res
+#if only a single record of type 1 exists
+if [ $(echo "${res}" | grep "^1" | wc -l) -eq 1 ] || \
+   [ $(echo "${res}" | wc -l) -eq 1 ]; then
+    aid=$(echo $res | cut -d '|' -f2)
+    ruby anidb.rb $aid
+    #eii -u -x -t master -c episodecount -f name -v '${name}' -n adata0
+    #eii -u -x -t master -c date -f name -v '${name}' -n adata1
 else
-  echo nop
+  echo nothing found
 fi
+
+#!!debug disabled
 #eidirpop "${1}" ${data}
