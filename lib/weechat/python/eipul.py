b924959219033588ef53aa510d13b3a996093e0b
Kevin Vollmer <works.kvollmer@gmail.com>
2014-09-30 01:28:19 -0400

    Message yaya if batch unsupported

--- lib/weechat/python/eipul.py
+++ lib/weechat/python/eipul.py
@@ -6,22 +6,14 @@ wc.register("eipsh", "chishiki", "0.0", "GPL3", "all teh dcc things", "", "")
 
 process_output = ""
 
-def listen(data, rem_calls):
-    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    sock.connect("/tmp/aaa")
-    sock.send(b"test")
+def send_message(msg):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("casper", 8889))
+    s.send("!")
 
-    fullmsg = sock.recv(1024)
-    serv = fullmsg.split(" ")[0]
-    chan = fullmsg.split(" ")[1]
-    msg = fullmsg.split(" ")[2]
-
-    wc.prnt("", fullmsg)
-
-    sock.close()
-
-    #buff = wc.info_get("irc_buffer", "rizon,#")
-    #wc.command(buff, msg)
+def batch_unsupported(data, signal, signal_data):
+    if "unsupported" in signal_data:
+        send_message("!")
     return wc.WEECHAT_RC_OK
 
 def xfer_end_do(data, signal, signal_data):
@@ -34,6 +26,5 @@ def xfer_end_do(data, signal, signal_data):
         os.popen('eiyfs "%s"' % local, 'r', 0)
     return wc.WEECHAT_RC_OK
 
+wc.hook_signal("weechat_pv", "batch_unsupported", "")
 wc.hook_signal("xfer_ended", "xfer_end_do", "")
-
-#wc.hook_timer(5 * 1000, 0, 0, "listen", "test")
