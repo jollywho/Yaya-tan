c8f3eb32f123136d929d844a854420e46895c8c5
Kevin Vollmer <works.kvollmer@gmail.com>
2014-10-03 22:58:38 -0400

    Incremental eiyfs -> anidb data

--- lib/scripts/eiyfs.sh
+++ lib/scripts/eiyfs.sh
@@ -10,7 +10,7 @@ else
 fi
 
 #!!debug disabled
-#eiinsert ${data}
+eiinsert ${data}
 
 if [ "$HOSTNAME" == casper ]; then
   EIIDIR='/usr/share/eii/lib'
@@ -22,13 +22,12 @@ EIICMD="${EIIDIR}/eii.sh"
 dbdir=/home/chishiki/qp/einibl/anidb/bin/anidb.db
 sql="-t titles -c type,aid,title -f title -v ${name}"
 res=$(. ${EIICMD} -s -db $dbdir $sql)
-echo $res
 #if only a single record of type 1 exists
 if [ $(echo "${res}" | grep "^1" | wc -l) -eq 1 ] || \
    [ $(echo "${res}" | wc -l) -eq 1 ]; then
     aid=$(echo $res | cut -d '|' -f2)
-    ruby anidb.rb $aid
-    #eii -u -x -t master -c episodecount -f name -v '${name}' -n adata0
+    ep=$(ruby anidb.rb $aid)
+    . ${EIICMD} -u -x -t master -c episodecount -f name -v ${name} -n $ep
     #eii -u -x -t master -c date -f name -v '${name}' -n adata1
 else
   echo nothing found
