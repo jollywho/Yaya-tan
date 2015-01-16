9fd849a1a26f09fb982ea87101a81fb77d67ba10
Kevin Vollmer <work.kvollmer@gmail.com>
2015-01-16 11:59:14 +0000

    YQS wc.xfer

---
+++ lib/weechat/python/yqs_xfer.py
@@ -0,0 +1,64 @@
+import weechat as wc
+import socket
+import os
+
+M_HOST = "melchior"
+M_PORT = 3333
+C_HOST = "casper"
+C_PORT = 8889
+
+wc.register("yqs_xfer", "chishiki", "1.0", "GPL3", "all teh dcc things", "", "") 
+wc.hook_command("yqs_xfer",
+         "run all the things that go ping",
+         "[-l]",
+         "...",
+         "-l",
+         "run","")
+
+def send_message(host,port,msg):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(msg)
+
+def request():
+    infolist = wc.infolist_get("xfer", "", "")
+    name = ""
+    if infolist:
+      while wc.infolist_next(infolist):
+         name += wc.infolist_string(infolist, "filename")
+         name += " "
+         name += wc.infolist_string(infolist, "status_string")
+         name += " "
+         name += wc.infolist_string(infolist, "bytes_per_sec")
+         name += " "
+         name += wc.infolist_string(infolist, "eta")
+         name += " "
+         name += wc.infolist_string(infolist, "pos")
+         name += " "
+         name += wc.infolist_string(infolist, "size")
+         name += "|"
+      name = name[:-1]
+      wc.infolist_free(infolist)
+      send_message(M_HOST, M_PORT, name)
+
+def run(data, buffer, args):
+    if args == "-l":
+        request()
+    return wc.WEECHAT_RC_OK
+
+def xfer_end_do(data, signal, signal_data):
+    wc.infolist_next(signal_data)
+    status = wc.infolist_string(signal_data, 'status_string')
+    filename = wc.infolist_string(signal_data, 'filename')
+    local = wc.infolist_string(signal_data, 'local_filename')
+    if status == "done":
+        os.popen('eiyfs "%s"' % local, 'r', 0)
+    return wc.WEECHAT_RC_OK
+
+def batch_unsupported(data, signal, signal_data):
+    if "unsupported" in signal_data:
+        send_message(C_HOST, C_PORT, "!")
+    return wc.WEECHAT_RC_OK
+
+wc.hook_signal("weechat_pv", "batch_unsupported", "")
+wc.hook_signal("xfer_ended", "xfer_end_do", "")
