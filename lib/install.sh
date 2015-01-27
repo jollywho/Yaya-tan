f1afb91226fcd6bc57e786165795347ab4422950
Kevin Vollmer <works.kvollmer@gmail.com>
2015-01-27 14:33:37 -0500

    Fix new req format

--- lib/install.sh
+++ lib/install.sh
@@ -7,4 +7,5 @@ cp scripts/eiyfs.sh            /usr/bin/eiyfs
 cp req_dcc.py                  /usr/bin/req_dcc
 cp yqs.sh                      /usr/bin/yqs
 cp yqs_get.sh                  /usr/bin/yqs_get
+cp ../einibl/einibl.sh         /usr/bin/einibl
 cp weechat/python/eipul.py     $HOME/.weechat/python/autoload/eipul.py
